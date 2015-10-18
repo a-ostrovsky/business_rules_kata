f96ba1330938257ead0d9155b027d35932b3d299
Alexander Ostrovsky <a-ostrovsky@users.noreply.github.com>
2015-10-18 11:28:56 +0200

    Receipt to royalty department action, refactoring

---
+++ src/main/java/com/kata/businessrules/payment/userDefinedRules/actions/ReceiptToRoyaltyDepartmentAction.java
@@ -0,0 +1,30 @@
+package com.kata.businessrules.payment.userDefinedRules.actions;
+
+import com.google.common.base.Preconditions;
+import com.kata.businessrules.Receipt;
+import com.kata.businessrules.ReceiptGenerator;
+import com.kata.businessrules.User;
+import com.kata.businessrules.products.Product;
+
+public class ReceiptToRoyaltyDepartmentAction implements Action {
+
+	private ReceiptGenerator receiptGenerator;
+	private User royaltyDepartment;
+
+	public ReceiptToRoyaltyDepartmentAction(
+			ReceiptGenerator receiptGenerator, User royaltyDepartment) {
+		Preconditions.checkNotNull(receiptGenerator);
+		Preconditions.checkNotNull(royaltyDepartment);
+		this.receiptGenerator = receiptGenerator;
+		this.royaltyDepartment = royaltyDepartment;
+	}
+	
+	@Override
+	public void execute(User customer, Product product) {
+		Preconditions.checkNotNull(customer);
+		Preconditions.checkNotNull(product);
+		Receipt receipt = receiptGenerator.generateReceipt(customer, product);
+		royaltyDepartment.issueReceipt(receipt);
+	}
+
+}
