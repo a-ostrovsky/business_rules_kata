ef9f4e446a2a14831d52a7ef9e518f981061e5bf
Alexander Ostrovsky <a-ostrovsky@users.noreply.github.com>
2015-10-21 08:08:57 +0200

    can parse product type filter from xml

--- src/main/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/ProductTypeFilterParser.java
+++ src/main/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/ProductTypeFilterParser.java
@@ -1,26 +1,45 @@
 package com.kata.businessrules.payment.userDefinedRules.xmlParsing;
 
+import java.util.Optional;
+import java.util.Set;
+
+import org.reflections.Reflections;
 import org.w3c.dom.Element;
 
-import com.google.common.base.Strings;
 import com.kata.businessrules.payment.userDefinedRules.filters.Filter;
+import com.kata.businessrules.payment.userDefinedRules.filters.ProductTypeFilter;
+import com.kata.businessrules.products.Product;
 
 public class ProductTypeFilterParser implements FilterParser {
 
-	@Override
-	public Filter parse(Element element) {
-		// TODO Auto-generated method stub
-		return null;
+	private Optional<Class<? extends Product>> getProductType(
+			String productTypeName) {
+		Reflections reflections = new Reflections("com.kata.businessrules");
+		Set<Class<? extends Product>> subTypes = reflections
+				.getSubTypesOf(Product.class);
+		Optional<Class<? extends Product>> productType = subTypes.stream()
+				.filter(productClass -> productClass.getSimpleName()
+						.equals(productTypeName))
+				.findFirst();
+		return productType;
 	}
 
 	@Override
-	public boolean canParse(Element element) {		
+	public Filter parse(Element element) {
+		String productTypeName = element.getAttribute("productType");		
+		Optional<Class<? extends Product>> productType = getProductType(productTypeName);		
+		return new ProductTypeFilter(productType.get());
+
+	}
+
+	@Override
+	public boolean canParse(Element element) {
 		boolean isNameCorrect = "whenPaidFor"
 				.equalsIgnoreCase(element.getTagName());
-		boolean hasProductTypeAttribute = !Strings
-				.isNullOrEmpty(element.getAttribute("productType"));
+		String productTypeName = element.getAttribute("productType");
+		boolean isProductTypeCorrect = getProductType(productTypeName).isPresent();
 		boolean hasOtherAttributes = element.getAttributes().getLength() != 1;
-		return isNameCorrect && hasProductTypeAttribute && !hasOtherAttributes;
+		return isNameCorrect && isProductTypeCorrect && !hasOtherAttributes;
 	}
 
 }
--- src/test/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/TestProductTypeFilterParser.java
+++ src/test/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/TestProductTypeFilterParser.java
@@ -4,12 +4,21 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.w3c.dom.Element;
 
+import com.kata.businessrules.ProductFixture;
 import com.kata.businessrules.XmlElement;
+import com.kata.businessrules.payment.userDefinedRules.filters.Filter;
+import com.kata.businessrules.products.Product;
 
 public class TestProductTypeFilterParser {
 
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+	
 	private ProductTypeFilterParser parser;
 
 	private void verifyCanParse(boolean expectedCanParse, String xmlElement)
@@ -24,8 +33,22 @@ public class TestProductTypeFilterParser {
 	}
 
 	@Test
+	public void parse_validType_productOfGivenType() throws Exception {
+		Product applicableProduct = ProductFixture.createSomeMembership();
+		Element element = XmlElement
+				.fromText("<whenPaidFor productType=\"Membership\"/>");
+		Filter filter = parser.parse(element);
+		assertThat(filter.isApplicable(applicableProduct), is(true));
+	}
+
+	@Test
 	public void canParse_onlyProductTypeAttribute_true() throws Exception {
 		verifyCanParse(true, "<whenPaidFor productType=\"Membership\"/>");
+	}
+	
+	@Test
+	public void canParse_productTypeThatDoesNotExist_false() throws Exception {
+		verifyCanParse(false, "<whenPaidFor productType=\"ProductTypeThatDoesNotExist\"/>");
 	}
 
 	@Test
@@ -44,5 +67,5 @@ public class TestProductTypeFilterParser {
 	@Test
 	public void canParse_notWhenPaidForFilter_false() throws Exception {
 		verifyCanParse(false, "<SOMETHING_ELSE productType=\"Video\"/>");
-	}	
+	}
 }
