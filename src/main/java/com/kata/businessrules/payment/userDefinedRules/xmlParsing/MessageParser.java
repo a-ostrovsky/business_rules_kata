a43d9a16f88802f51adf235d91c9b2e4251e37d5
Alexander Ostrovsky <a-ostrovsky@users.noreply.github.com>
2015-10-28 07:53:56 +0100

    MessageParser implemented

--- src/main/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/MessageParser.java
+++ src/main/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/MessageParser.java
@@ -2,20 +2,21 @@ package com.kata.businessrules.payment.userDefinedRules.xmlParsing;
 
 import org.w3c.dom.Element;
 
-import com.google.common.base.Preconditions;
 import com.kata.businessrules.contact.Message;
 
 public class MessageParser implements Parser<Message> {
 
 	@Override
 	public Message parse(Element element) {
-		Preconditions.checkNotNull(element);
-		return null;
+		// TODO: To return something more meaningful I need to know how a
+		// message looks like. E.g. formatted / plain text, ...
+		return new Message() {
+		};
 	}
 
 	@Override
 	public boolean canParse(Element element) {
-		if(element == null) {
+		if (element == null) {
 			return false;
 		}
 		return "message".equalsIgnoreCase(element.getTagName());
