548f385fd2ea53ae6d2f2cdecf379e4b0bc51165
Alexander Ostrovsky <a-ostrovsky@users.noreply.github.com>
2015-10-29 08:39:18 +0100

    Refactoring

--- src/main/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/IssueReceiptActionParser.java
+++ src/main/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/IssueReceiptActionParser.java
@@ -1,7 +1,5 @@
 package com.kata.businessrules.payment.userDefinedRules.xmlParsing;
 
-import java.util.Collection;
-
 import org.w3c.dom.Element;
 
 import com.google.common.base.Preconditions;
@@ -13,30 +11,25 @@ import com.kata.businessrules.products.Product;
 
 public class IssueReceiptActionParser implements Parser<Action> {
 
-	private Collection<Parser<Selector<User>>> userSelectorParsers;
-	private Collection<Parser<Selector<Product>>> productSelectorParsers;
+	private Parser<Selector<User>> userSelectorParser;
+	private Parser<Selector<Product>> productSelectorParser;
 	private IssueReceiptActionFactory factory;
 
 	public IssueReceiptActionParser(IssueReceiptActionFactory factory,
-			Collection<Parser<Selector<User>>> userSelectorParsers,
-			Collection<Parser<Selector<Product>>> productSelectorParsers) {
-		Preconditions.checkNotNull(userSelectorParsers);
-		Preconditions.checkNotNull(productSelectorParsers);
+			Parser<Selector<User>> userSelectorParser,
+			Parser<Selector<Product>> productSelectorParser) {
+		Preconditions.checkNotNull(userSelectorParser);
+		Preconditions.checkNotNull(productSelectorParser);
 		Preconditions.checkNotNull(factory);
 		this.factory = factory;
-		this.userSelectorParsers = userSelectorParsers;
-		this.productSelectorParsers = productSelectorParsers;
+		this.userSelectorParser = userSelectorParser;
+		this.productSelectorParser = productSelectorParser;
 	}
 
 	@Override
 	public Action parse(Element element) {
 		Preconditions.checkNotNull(element);
-		Parser<Selector<User>> userSelectorParser = userSelectorParsers.stream()
-				.filter(parser -> parser.canParse(element)).findFirst().get();
 		Selector<User> userSelector = userSelectorParser.parse(element);
-		Parser<Selector<Product>> productSelectorParser = productSelectorParsers
-				.stream().filter(parser -> parser.canParse(element)).findFirst()
-				.get();
 		Selector<Product> productSelector = productSelectorParser
 				.parse(element);
 		return factory.create(userSelector, productSelector);
@@ -49,10 +42,8 @@ public class IssueReceiptActionParser implements Parser<Action> {
 		}
 		boolean isNameCorrect = "receiptFor"
 				.equalsIgnoreCase(element.getTagName());
-		boolean canParseReceiver = userSelectorParsers.stream()
-				.anyMatch(parser -> parser.canParse(element));
-		boolean canParseProduct = productSelectorParsers.stream()
-				.anyMatch(parser -> parser.canParse(element));
+		boolean canParseReceiver = userSelectorParser.canParse(element);
+		boolean canParseProduct = productSelectorParser.canParse(element);
 		return isNameCorrect && canParseReceiver && canParseProduct;
 	}
 
--- src/test/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/TestIssueReceiptActionParser.java
+++ src/test/java/com/kata/businessrules/payment/userDefinedRules/xmlParsing/TestIssueReceiptActionParser.java
@@ -6,17 +6,22 @@ import static org.hamcrest.MatcherAssert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Element;
 
 import com.kata.businessrules.User;
+import com.kata.businessrules.XmlElement;
 import com.kata.businessrules.payment.userDefinedRules.actions.Action;
 import com.kata.businessrules.payment.userDefinedRules.actions.IssueReceiptActionFactory;
 import com.kata.businessrules.payment.userDefinedRules.actions.Selector;
 import com.kata.businessrules.products.Product;
 
 public class TestIssueReceiptActionParser {
-	private IssueReceiptActionParserContext parserContext;
 	private IssueReceiptActionParser parser;
 	private IssueReceiptActionFactory actionFactory;
+	private Parser<Selector<User>> userSelectorParser;
+	private Parser<Selector<Product>> productSelectorParser;
+	private Selector<Product> productSelector;
+	private Selector<User> userSelector;
 
 	private Action setupIssueReceiptActionFactory(Selector<User> userSelector,
 			Selector<Product> productSelector) {
@@ -26,102 +31,68 @@ public class TestIssueReceiptActionParser {
 		return result;
 	}
 
-	private static Selector<User> setupUserSelector(
-			IssueReceiptActionParserContext context) {
-		Parser<Selector<User>> userSelectorParser = context
-				.setSomeParserCanParseReceiver();
-		Selector<User> userSelector = userSelectorParser
-				.parse(context.getXml());
-		return userSelector;
-	}
-
-	private static Selector<Product> setupProductSelector(
-			IssueReceiptActionParserContext context) {
-		Parser<Selector<Product>> productSelectorParser = context
-				.setSomeParserCanParseProduct();
-		Selector<Product> productSelector = productSelectorParser
-				.parse(context.getXml());
-		return productSelector;
-	}
-
+	@SuppressWarnings("unchecked")
 	@Before
 	public void setup() {
+		userSelector = (Selector<User>) mock(Selector.class);
+		productSelector = (Selector<Product>) mock(Selector.class);
 		actionFactory = mock(IssueReceiptActionFactory.class);
-		parserContext = new IssueReceiptActionParserContext(actionFactory);
-		parser = parserContext.getParser();
+		userSelectorParser = (Parser<Selector<User>>) mock(Parser.class);
+		productSelectorParser = (Parser<Selector<Product>>) mock(Parser.class);
+		parser = new IssueReceiptActionParser(actionFactory, userSelectorParser,
+				productSelectorParser);
 	}
 
 	@Test
 	public void parse_canParse_IssueReceiptActionAsDescribedInXml()
 			throws Exception {
-		parserContext.setXml(
+		Element xml = XmlElement.fromText(
 				"<receiptFor product=\"bought\" receiver=\"customer\" />");
-		Selector<Product> productSelector = setupProductSelector(parserContext);
-		Selector<User> userSelector = setupUserSelector(parserContext);
+		when(productSelectorParser.parse(xml)).thenReturn(productSelector);
+		when(userSelectorParser.parse(xml)).thenReturn(userSelector);
 		Action expectedResult = setupIssueReceiptActionFactory(userSelector,
 				productSelector);
-		Action action = parser.parse(parserContext.getXml());
+		Action action = parser.parse(xml);
 		assertThat(action, is(expectedResult));
 	}
 
 	@Test
 	public void canParse_canParseReceiverAndProduct_true() throws Exception {
-		parserContext.setXml(
+		Element xml = XmlElement.fromText(
 				"<receiptFor product=\"bought\" receiver=\"customer\" />");
-		parserContext.setSomeParserCanParseProduct();
-		parserContext.setSomeParserCanParseReceiver();
-		parserContext.verifyCanParse();
+		when(productSelectorParser.canParse(xml)).thenReturn(true);
+		when(userSelectorParser.canParse(xml)).thenReturn(true);
+		ParserAssert.canParse(parser, xml);
 	}
 
 	@Test
 	public void canParse_canNotParseReceiver_false() throws Exception {
-		parserContext.setXml("<receiptFor product=\"bought\" />");
-		parserContext.setSomeParserCanParseProduct();
-		parserContext.setNoParsersCanParseReceiver();
-		parserContext.verifyCannotParse();
+		Element xml = XmlElement.fromText("<receiptFor product=\"bought\" />");
+		when(productSelectorParser.canParse(xml)).thenReturn(true);
+		when(userSelectorParser.canParse(xml)).thenReturn(false);
+		ParserAssert.cannotParse(parser, xml);
 	}
 
 	@Test
 	public void canParse_canNotParseProduct_false() throws Exception {
-		parserContext.setXml("<receiptFor receiver=\"customer\" />");
-		parserContext.setSomeParserCanParseReceiver();
-		parserContext.setNoParsersCanParseProduct();
-		parserContext.verifyCannotParse();
+		Element xml = XmlElement
+				.fromText("<receiptFor receiver=\"customer\" />");
+		when(productSelectorParser.canParse(xml)).thenReturn(false);
+		when(userSelectorParser.canParse(xml)).thenReturn(true);
+		ParserAssert.cannotParse(parser, xml);
 	}
 
 	@Test
 	public void canParse_notReceiptForAction_false() throws Exception {
-		parserContext.setXml(
+		Element xml = XmlElement.fromText(
 				"<SOMETHING_ELSE product=\"bought\" receiver=\"customer\" />");
-		parserContext.setSomeParserCanParseProduct();
-		parserContext.setSomeParserCanParseReceiver();
-		parserContext.verifyCannotParse();
+		when(productSelectorParser.canParse(xml)).thenReturn(true);
+		when(userSelectorParser.canParse(xml)).thenReturn(true);
+		ParserAssert.cannotParse(parser, xml);
 	}
 
 	@Test
 	public void canParse_null_false() throws Exception {
-		parserContext.setXml(null);
-		parserContext.verifyCannotParse();
+		ParserAssert.cannotParse(parser, (Element) null);
 	}
-
-	/*
-	 * @Test public void canParse_receiverAttributeIsNotCustomer_false() throws
-	 * Exception { ParserAssert.cannotParse(parser,
-	 * "<receiptFor product=\"bought\" receiver=\"something_else\" />"); }
-	 * 
-	 * @Test public void canParse_receiverIdIsGiven_false() throws Exception {
-	 * ParserAssert.canParse(parser,
-	 * "<receiptFor product=\"bought\" receiverId=\"royaltyDepartment\" />"); }
-	 * 
-	 * @Test public void
-	 * canParse_thereAreAnotherAttriburesThanReceiverAndProduct_false() throws
-	 * Exception { ParserAssert.cannotParse(parser,
-	 * "<receiptFor product=\"bought\" receiver=\"customer\" " +
-	 * "somethingElse=\"true\" />"); }
-	 * 
-	 * @Test public void canParse_productIsMissing_false() throws Exception {
-	 * ParserAssert.cannotParse(parser, "<receiptFor receiver=\"customer\" />");
-	 * }
-	 * 
-	 */
 }
